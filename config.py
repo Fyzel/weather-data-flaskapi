f354dd2fc681f48a8f7b385b7b1a9b343355c717
Fyzel <fyzel@users.noreply.github.com>
2018-09-21 20:59:36 -0600

    Minor heading change.

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+"""
+@author:     Fyzel@users.noreply.github.com
+
+@copyright:  2017 Englesh.org. All rights reserved.
+
+@license:    https://github.com/Fyzel/weather-data-flaskapi/blob/master/LICENSE
+
+@contact:    Fyzel@users.noreply.github.com
+@deffield    updated: 2017-06-14
+"""
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
